ebdfb5172ad91e32b5005ba0a1062a1cbdf6ebc2
Periyandavar Ganesh <vickyperiyandavar@gmail.com>
2025-03-15 11:23:09 +0530

    feat: add loader classes

---
+++ Container.php
@@ -0,0 +1,235 @@
+<?php
+
+namespace System\Loader;
+
+use Exception;
+use ReflectionClass;
+use ReflectionNamedType;
+use ReflectionParameter;
+use System\Core\Log;
+
+class Container
+{
+    private static $instances = [];
+    private static $services = [];
+
+    /**
+     * Set the instance and service in the container.
+     *
+     * @param string $_name
+     * @param mixed $_closure
+     * @param bool  $_singleton
+     */
+    public static function set(string $_name, mixed $_closure, bool $_singleton = false)
+    {
+        $name = strtolower($_name);
+        if ($_singleton) {
+            self::$instances[$name] = $_closure;
+        } else {
+            self::$services[$name] = $_closure;
+        }
+    }
+
+    /**
+     * Resolve the dependency.
+     *
+     * @param ReflectionParameter $_param
+     *
+     * @return mixed
+     */
+    private static function resolveDependency(ReflectionParameter $_param)
+    {
+        $type = $_param->getType();
+
+        if ($type instanceof ReflectionNamedType && ! $type->isBuiltin()) {
+            $name = $type->getName();
+
+            return self::isClassRegistered($name) ? self::get($name) : self::resolve($name);
+        }
+
+        if ($_param->isDefaultValueAvailable()) {
+            return $_param->getDefaultValue();
+        }
+
+        return null;
+    }
+
+    /**
+     * Return the instance.
+     *
+     * @param string $_name
+     */
+    public static function getInstance(string $_name)
+    {
+        $instance = self::$instances[$_name] ?? null;
+
+        if (is_callable($instance)) {
+            $instance = self::$instances[$_name]();
+            self::$instances[$_name] = $instance;
+        }
+
+        return $instance;
+    }
+
+    /**
+     * Return the service.
+     *
+     * @param string $_name
+     */
+    public static function getService(string $_name)
+    {
+        $service = self::$services[$_name] ?? null;
+
+        if (is_callable($service)) {
+            return $service();
+        }
+
+        return $service;
+    }
+
+    /**
+     * Get the object from the container.
+     *
+     * @param string $_name
+     *
+     * @return mixed
+     */
+    public static function get(string $_name)
+    {
+        $name = strtolower($_name);
+        $object = self::getInstance($name) ?? self::getService($_name);
+
+        if (is_null($object)) {
+            throw new Exception("Service not found: {$_name}");
+        }
+
+        return $object;
+    }
+
+    /**
+     * Check if the class is registered.
+     *
+     * @param string $_name
+     *
+     * @return bool
+     */
+    public static function isClassRegistered(string $_name)
+    {
+        $name = strtolower($_name);
+
+        return isset(self::$instances[$name]) || isset(self::$services[$name]);
+    }
+
+    /**
+     * Resolve the class.
+     *
+     * @param string $_class_name
+     *
+     * @return mixed
+     */
+    public static function resolve(string $_class_name)
+    {
+        $reflection = new ReflectionClass($_class_name);
+        if ($reflection->isAbstract() || $reflection->isInterface()) {
+            Log::getInstance()->info("{$_class_name} is abstract or interface");
+
+            return null;
+        }
+
+        $constructor = $reflection->getConstructor();
+
+        $params = $constructor ? $constructor->getParameters() : [];
+
+        $dependencies = [];
+        foreach ($params as $param) {
+            $dependencies[] = self::resolveDependency($param);
+        }
+
+        return $reflection->newInstanceArgs($dependencies);
+    }
+
+    /**
+     * Load the configuration.
+     *
+     * @param array $_config
+     */
+    public static function loadFromConfig(array $_config)
+    {
+        foreach ($_config as $name => $config) {
+            $singleton = $config['singleton'] ?? false;
+            $closure = function () use ($config) {
+                if ($config['params']) {
+                    $params = self::getClassParams($config['params']);
+
+
+                    return new $config['class'](...$params);
+                }
+
+
+                return new $config['class']();
+            };
+            self::set($name, $closure, $singleton);
+        }
+    }
+
+    /**
+     * Get the class params.
+     *
+     * @param array $_params
+     *
+     * @return array
+     */
+    public static function getClassParams(array $_params)
+    {
+        $params = [];
+
+        foreach ($_params as $param => $value) {
+
+            if (is_object($value)) {
+                $params[$param] = $value;
+
+                continue;
+            }
+
+            if (is_callable($value)) {
+                $params[$param] = $value();
+
+                continue;
+            }
+
+            if (! is_string($value)) {
+                $params[$param] = $value;
+
+                continue;
+            }
+
+            if (str_starts_with(strtoupper($value), '\s')) {
+                $params[$param] = $value;
+
+                continue;
+            }
+
+            if (str_ends_with($value, '_bli')) {
+                $params[$param] = self::getBLI($value);
+
+                continue;
+            }
+
+            if (str_ends_with($value, '_ali')) {
+                $params[$param] = self::getALI($value);
+
+                continue;
+            }
+
+            if (self::isClassRegistered($value)) {
+                $params[$param] = self::get($value);
+
+                continue;
+            }
+
+            $_params[$param] = $value;
+        }
+
+        return $params;
+    }
+}
---
+++ Loader.php
@@ -0,0 +1,240 @@
+<?php
+/**
+ * Loader
+ * php version 7.3.5
+ *
+ * @category Loader
+ * @package  Core
+ * @author   Periyandavar <periyandavar@gmail.com>
+ * @license  http://license.com license
+ * @link     http://url.com
+ */
+
+namespace System\Loader;
+
+use System\Core\SysController;
+use System\Core\Utility;
+use System\Core\FrameworkException;
+
+defined('VALID_REQ') or exit('Invalid request');
+/**
+ * Loader Class autoloads the files
+ *
+ * @category Loader
+ * @package  Core
+ * @author   Periyandavar <periyandavar@gmail.com>
+ * @license  http://license.com license
+ * @link     http://url.com
+ */
+class Loader
+{
+    /**
+     * Loader class instance
+     *
+     * @var Loader|null $_instance
+     */
+    private static $_instance = null;
+
+    private $_prefixes = [];
+    /**
+     * Controller object
+     *
+     * @var Loader
+     */
+    private static $_ctrl;
+
+    /**
+     * Instantiate the the Loader instance
+     */
+    private function __construct()
+    {
+        global $config;
+        $this->_prefixes = [
+            'model' => $config['model'] ?? "App\Model\\",
+            'service' => $config['service']  ?? "App\Service\\",
+            'helper' => $config['helper'] ?? "App\Helper\\",
+            'library' => $config['library'] ?? "System\Library\\",
+        ];
+
+        $this->loadAll('src/app/config/routes');
+    }
+
+    /**
+     * Loads the all classes from autoload class list
+     * and creates the instance for them
+     *
+     * @param SysController $ctrl Controller object
+     *
+     * @return Loader
+     * @throws FrameworkException
+     */
+    public static function autoLoadClass(SysController $ctrl, $autoloads): Loader
+    {
+        global $autoload;
+        $autoloads = $autoloads ?? $autoload;
+        $loads = ['model', 'service', 'library', 'helper'];
+        static::$_instance ?? static::intialize();
+        static::$_ctrl = $ctrl;
+        foreach ($loads as $load) {
+            $files = $autoloads[$load];
+            is_array($files) or $files = array($files);
+            static::$_instance->$load(...$files);
+        }
+
+        return static::$_instance;
+    }
+
+    /**
+     * Loads models
+     *
+     * @param string ...$models Model list
+     *
+     * @return void
+     * @throws FrameworkException
+     */
+    public function model(...$models)
+    {
+        $ns = $this->_prefixes['model'];
+        foreach ($models as $model) {
+            $class = $ns . $model . 'Model';
+            if (class_exists($class)) {
+                static::$_ctrl->{lcfirst($model)} = new $class();
+            } else {
+                throw new FrameworkException(
+                    "Unable to locate the model class '$model'"
+                );
+            }
+        }
+    }
+
+    /**
+     * Loads Services
+     *
+     * @param string ...$services Service list
+     *
+     * @return void
+     * @throws FrameworkException
+     */
+    public function service(...$services)
+    {
+        $ns = $this->_prefixes['service'];
+        foreach ($services as $service) {
+            $class = $ns . $service . 'Service';
+            if (class_exists($class)) {
+                static::$_ctrl->{lcfirst($service)} = new $class();
+            } else {
+                throw new FrameworkException(
+                    "Unable to loacate the '$service' class"
+                );
+            }
+        }
+    }
+
+    /**
+     * Loads Libraries
+     *
+     * @param string ...$libraries Library list
+     *
+     * @return void
+     * @throws FrameworkException
+     */
+    public function library(...$libraries)
+    {
+        $ns = $this->_prefixes['library'];
+        foreach ($libraries as $library) {
+            $sys_lib_class = "System\\Library\\" . $library;
+            $cust_lib_class = $ns . $library;
+            if (class_exists($sys_lib_class)) {
+                static::$_ctrl->{lcfirst($library)} = new $sys_lib_class();
+            } elseif (class_exists($cust_lib_class)) {
+                static::$_ctrl->{lcfirst($library)} = new $cust_lib_class();
+            } else {
+                throw new FrameworkException("Library class '$library' not found");
+            }
+        }
+    }
+
+
+    /**
+     * Loads helpers
+     *
+     * @param string ...$helpers Helper list
+     *
+     * @return void
+     * @throws FrameworkException
+     */
+    public function helper(...$helpers)
+    {
+        foreach ($helpers as $helper) {
+            // $helper = (Utility::endswith($helper, "helper")
+            //     ? $helper
+            //     : $helper . '.php');
+            // $helper = ucfirst($helper);
+                // var_export([$this->_prefixes['helper'] . '/' . $helper,'src/system/helper/' . $helper ]);//exit;
+            // if (file_exists($this->_prefixes['helper'] . $helper)) {
+            //     include_once $this->_prefixes['helper'] . '/' . $helper;
+            // } elseif (file_exists('src/system/helper/' . $helper)) {
+            //     include_once 'src/system/helper/' . $helper;
+            // } else {
+            //     throw new FrameworkException("Helper class '$helper' not found");
+            // }
+            // $helper = ucfirst($helper);
+            $helper_file = APP_DIR . '/src/system/helper/' . $helper . '.php';
+            if (class_exists($this->_prefixes['helper'] . $helper)) {
+                //
+            } elseif (file_exists($helper_file)) {
+                include_once $helper_file;
+            } else {
+
+                throw new FrameworkException("Helper class '$helper' not found");
+            }
+        }
+    }
+
+    /**
+     * Loads all php files from the specified directory
+     *
+     * @param string $dir Directory Name
+     *
+     * @return void
+     */
+    public function loadAll(string $dir)
+    {
+        foreach (glob("$dir/*.php") as $filename) {
+            include_once $filename;
+        }
+    }
+
+
+    /**
+     * Includes the file if it exists
+     *
+     * @param string      $file   file name
+     *
+     * @return bool
+     */
+    public function loadFile(string $file): bool
+    {
+        $file = rtrim($file, '.php') . '.php';
+
+        if (file_exists($file)) {
+            include_once $file;
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Intialize the Loader class and returns load class object
+     *
+     * @return Loader
+     */
+    public static function intialize(): Loader
+    {
+        if (self::$_instance == null) {
+            self::$_instance = new static();
+        }
+        return self::$_instance;
+    }
+}
