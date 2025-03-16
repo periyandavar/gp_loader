91649b1f1741a4a12a2b73e929bb7b743e6b1e12
Periyandavar Ganesh <50901377+periyandavar@users.noreply.github.com>
2025-03-16 15:59:02 +0530

    Revert "Revert "feat: add testcases""

--- src/Config/ConfigLoader.php
+++ src/Config/ConfigLoader.php
@@ -30,6 +30,8 @@ abstract class ConfigLoader
 
     protected $loadHandler;
 
+    protected $data = [];
+
     public const ENV_LOADER = 'env';
     public const ARRAY_LOADER = 'array';
 
@@ -46,8 +48,38 @@ abstract class ConfigLoader
 
     public function load()
     {
-        $data = $this->innerLoader();
-        $this->loadHandler($data);
+        $this->data = $this->innerLoader();
+
+        $this->loadHandler();
+    }
+
+    public function get(string $key)
+    {
+        return $this->data[$key] ?? null;
+    }
+
+    public function getAll()
+    {
+        return $this->data;
+    }
+
+    public function merge(array $data)
+    {
+        $this->data = array_merge($this->data, $data);
+    }
+
+    public function set(string $key, $value, bool $strict = false)
+    {
+        if ($strict && !isset($this->data[$key])) {
+            throw new Exception('Key not found : ' . $key);
+        }
+
+        $this->data[$key] = $value;
+    }
+
+    public function override(array $data)
+    {
+        $this->data = $data;
     }
 
     abstract public function innerLoader(): array;
@@ -64,13 +96,13 @@ abstract class ConfigLoader
         }
     }
 
-    private function loadHandler($data)
+    private function loadHandler()
     {
         if ($this->loadHandler) {
-            return call_user_func($this->loadHandler, $data);
+            return call_user_func($this->loadHandler, $this->data);
         }
 
-        $this->defaultHandler($data);
+        $this->defaultHandler($this->data);
     }
 
     public function setLoadHandler(callable $_loadHandler)
